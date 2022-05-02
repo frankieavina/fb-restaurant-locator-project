f34798c8ac5d012233615921e4b7336d6d60c70b
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-05-02 17:51:37 -0400

    adding the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import SearchIcon from '@material-ui/core/searchIcon' ;
 
-const Header = () =>{
-    return(
-        <div>
-            header
+const Header = () => {
+  return (
+    <div className="headerRaper">
+      <div className="title">
+        <h1>Restaurant Search Columbus, Ohio:</h1>
+      </div>
+      <div className="search">
+        <div className="searchIcon">
+          <SearchIcon />
         </div>
-    )
-
-}
+        <inputbase placeholder="Search......"
+        />
+      </div>
+    </div>
+  );
+};
 export default Header;
