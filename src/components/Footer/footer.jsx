9f4dbb421a499ea9cc04ef0096648d30ce0e2256
frankieavina <frankieavina@gmail.com>
2022-05-02 15:55:40 -0700

    finishing footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+// adding material icon 
+import AppleIcon from '@material-ui/icons/Apple';
 
 const FooterWrapper = styled.footer`
   background-color: #333;
@@ -12,6 +14,7 @@ const Footer = () => {
   return (
     <FooterWrapper>
       Betelihem A Hayle and Frankie Avina © 2022
+      <AppleIcon/>
     </FooterWrapper>
   );
 };
