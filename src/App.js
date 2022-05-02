7a7f874c46713db6a766feb649328e58bcd7b90c
frankieavina <frankieavina@gmail.com>
2022-05-02 11:46:21 -0700

    adding routing and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
+import { useState } from 'react';
 import './App.css';
+import LocationContext from './context/LocationContext';
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/Home/home';
+import PlaceDetails from './components/placeDetails'
 
 function App() {
+
+  const [locations, setLocations] = useState([]); 
+
   return (
     <div className="App">
+      {/* providing context  */}
+      <LocationContext.Provider value={{locations:locations}}>
 
-     
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path="placedetails/:id" element={<PlaceDetails/>} />
+            <Route path="*" element={<NotFound />} />
+          </Route>          
+        </Routes>
+
+      </LocationContext.Provider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () =>{
     return(
         <div>
-            
+            header
         </div>
     )
 
