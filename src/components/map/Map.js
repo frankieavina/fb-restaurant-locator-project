0c0991da2d7a85c67eb3071c90d4a8f8490803f2
frankieavina <frankieavina@gmail.com>
2022-05-03 13:32:10 -0700

    Maps Funtionally working

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,49 @@
+import React from 'react';
+// import GoogleMapReact from 'google-map-react'; 
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow
+} from '@react-google-maps/api'; 
 
+
+const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw",
+};
+// const options = {
+//     styles: mapStyles,
+//     disableDefaultUI: true,
+//     zoomControl: true,
+// };
+const center = {
+    lat: 43.6532,
+    lng: -79.3832,
+};
+const libraries = ['places'];
 
 const Map = () =>{
+    
+
+
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY, 
+        libraries
+    });
+
+    if (loadError) return console.log("Error Loading maps");
+    if (!isLoaded) return console.log("Loading Maps");
+
     return(
         <div>
-            
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+            >
+
+            </GoogleMap>
         </div>
     )
 
