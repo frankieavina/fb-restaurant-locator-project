8f392e6bce156f633f9282fe55e58cc4023d2f0d
frankieavina <frankieavina@gmail.com>
2022-05-09 15:33:18 -0700

    Details Page Complete

--- src/components/list/ListCard.js
+++ src/components/list/ListCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 
+
 function ListCard({name, description, rating, phone, address,location_id}) {
 
     //navigate to different page with Router useNavigate
     const navigate = useNavigate(); 
 
   const onHandleClick = () => {
-    console.log('Params ID:',parseInt(location_id));
     navigate(`/place-details/${parseInt(location_id)}`);
   }
 
--- src/components/placeDetails/PlaceDetails.js
+++ src/components/placeDetails/PlaceDetails.js
@@ -1,46 +1,115 @@
 import React, {useContext} from "react";
 // importing Context (location context)
 import LocationContext from "../../context/LocationContext";
-import { Route, useParams } from 'react-router'; 
+import { Route, useParams, useNavigate } from 'react-router'; 
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import LanguageIcon from '@material-ui/icons/Language';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styled from "styled-components";
+import Rating from '@mui/material/Rating';
+import '../../App.css';
 
 
 const RestaurantCard = styled.div`
 border: 1px solid lightgray;
-border-radius: 10px;
-padding: 1.5rem;
+border-radius: 1rem;
+padding: 1rem;
+background-color: white; 
+display: flex; 
+width: 90%; 
+margin: 2rem auto;
+
+
+a{
+    color: blue;
+    text-decoration: underline; 
+}
+a:hover{
+    cursor: pointer;
+    color: pink;
+    text-decoration: none;
+}
+.back-home{
+    color: blue; 
+}
+.back-home:hover{
+    cursor: pointer; 
+    color: pink; 
+}
+.icon{
+    color: light-gray; 
+}
+img{
+    width:300px;
+    height:350px; 
+    margin: 1rem;
+}
+img-wrapper{
+    width: 50%;
+}
+.details-info{
+    text-align: left !important; 
+}
+h5.open{
+    color: green; 
+}
+h5.closed{
+    color: red; 
+}
+.open{
+    color: green;
+}
+.closed{
+    color: orange; 
+}
+textarea{
+    border: none; 
+    overflow-y: scroll;
+}
 `
 
 const PlaceDetails = () =>{
 
-    let isOpen = 'Open'; 
+    let isOpen = 'OPEN'; 
     const { id: restId } = useParams();
     const { restaurants } = useContext(LocationContext); 
+
+    //navigate to different page with Router useNavigate
+    const navigate = useNavigate(); 
     
-    console.log('rest id:', restId)
     // return the object of the restaurant who's id matches with the param id (eg.place-details/819237)
     const restaurant =  restaurants.find(({location_id}) =>  location_id == restId ); 
     // is restaurant open 
-    ( !restaurant.is_closed )?( isOpen = 'Open'):( isOpen = 'Closed');
+    ( !restaurant.is_closed )?( isOpen = 'OPEN'):( isOpen = 'CLOSED');
 
     return(
-        <RestaurantCard>
-            <div>
-                <img src={restaurant.photo?.images.large.url} alt="Picture of Restaurant"/>
-            </div>
-            <div>
-                <h1>{restaurant.name}</h1>
-                <h3>{isOpen}</h3>
-                <p>Rating:{restaurant.rating}</p>
-                <hr/>
-                <p>{restaurant.address}</p>
-                <p>{restaurant.phone}</p>
-                <p>{restaurant.website}</p>
-                <hr/>
-                <p>{restaurant.description}</p>
+        <div className="details-page">
+            <RestaurantCard>
+                <div className="img-wrapper">
+                    <img src={restaurant.photo?.images.large.url} alt='Picture for Restaurant Not Available'/>
+                </div>
+                <div className="details-info">
+                    <div className='back-home' onClick={() => navigate(`/`)}>
+                        <p><span className="icon"><ArrowBackIcon/></span>back to home</p>
+                    </div>
+                    <h3>{restaurant.name}</h3>
+                    <div className={(isOpen=='OPEN')? 'open':'closed'} >
+                        <h5>CURRENTLY {isOpen}</h5>
+                    </div>
+                    <Rating name="read-only" value={restaurant.rating} readOnly />
+                    <hr/>
+                    <p><span className="icon"><LocationOnIcon viewBox="0 0 25 25"/></span>{restaurant.address}</p>
+                    <p><span className="icon"><PhoneIcon/></span>{restaurant.phone}</p>
+                    <a><span className="icon"><LanguageIcon/></span>{restaurant.website}</a>
+                    <hr/>
+                    <p>Description:</p>
+                    <textarea rows="8" cols="50">{restaurant?.description}</textarea>
 
-            </div>
-        </RestaurantCard>
+                </div>
+            </RestaurantCard>        
+        </div>
+
     );
 
 }
